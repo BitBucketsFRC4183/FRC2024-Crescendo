c9f2d16c7fed4d28568be2cb1c093ca2e1e2e4b0
arjuncaputo <arjuncaputo2025@gmail.com>
2024-01-05 00:08:07 -0700

    vision template draft1

--- src/main/java/org/bitbuckets/Robot.java
+++ src/main/java/org/bitbuckets/Robot.java
@@ -1,6 +1,10 @@
 package org.bitbuckets;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.wpilibj.TimedRobot;
+import org.bitbuckets.vision.VisionSubsystem;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
 import xyz.auriium.mattlib2.log.MattLog;
 
 public class Robot extends TimedRobot {
--- src/main/java/org/bitbuckets/vision/VisionSubsystem.java
+++ src/main/java/org/bitbuckets/vision/VisionSubsystem.java
@@ -1,19 +1,68 @@
 package org.bitbuckets.vision;
 
+import edu.wpi.first.apriltag.AprilTagDetector;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.math.geometry.Pose3d;
 import org.bitbuckets.Robot;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+
+import java.util.Optional;
 
 public class VisionSubsystem {
 
-    static final VisionComponent COMPONENT = Robot.LOG.loadWaiting(VisionComponent.class, "vision");
-    static final VisionComponent COMPONENT2 = Robot.LOG.loadWaiting(VisionComponent.class, "vision-2");
+    final PhotonCamera camera_1;
+    final PhotonCamera camera_2;
+    final AprilTagFieldLayout layout;
+    final PhotonPoseEstimator estimator;
+    final AprilTagDetector aprilTagDetector;
 
-    static final int id = COMPONENT.id();
+    static final VisionComponent COMPONENT = Robot.LOG.loadWaiting(VisionComponent.class, "vision");
+
+    public VisionSubsystem(PhotonCamera camera_1, PhotonCamera camera_2, AprilTagFieldLayout layout, PhotonPoseEstimator estimator, AprilTagDetector aprilTagDetector) {
+        this.camera_1 = camera_1;
+        this.camera_2 = camera_2;
+        this.layout = layout;
+        this.estimator = estimator;
+        this.aprilTagDetector = aprilTagDetector;
+    }
 
     public void init() {
+        //example logging DELETE
         int id = COMPONENT.id();
         int result = id*4;
         COMPONENT.x_position(result);
+        //frc using 36h11 fam this year
+        aprilTagDetector.addFamily("36h11");
     }
+
+
+    //BASIC INFORMATION GATHERING FROM CAMERAS
+    public Optional<Pose3d> estimateBestVisionTarget_1() {
+
+        return Optional.ofNullable(
+                camera_1.getLatestResult().getBestTarget()
+        ).flatMap(tgt -> layout.getTagPose(tgt.getFiducialId()));
+
+    }
+
+    public Optional<Pose3d> estimateBestVisionTarget_2() {
+
+        return Optional.ofNullable(
+                camera_2.getLatestResult().getBestTarget()
+        ).flatMap(tgt -> layout.getTagPose(tgt.getFiducialId()));
+
+    }
+
+    public Optional<Pose3d> estimateVisionRobotPose_1() {
+        return estimator.update(camera_1.getLatestResult()).map(poseDat -> poseDat.estimatedPose);
+    }
+
+    public Optional<Pose3d> estimateVisionRobotPose_2() {
+        return estimator.update(camera_2.getLatestResult()).map(poseDat -> poseDat.estimatedPose);
+    }
+
+
+
 
 }
