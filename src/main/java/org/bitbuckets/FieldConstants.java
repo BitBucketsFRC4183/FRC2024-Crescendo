2c15ceb612b6809ed7bb9a6a58d4838890af446d
Matt Lui <92664603+superyuuki@users.noreply.github.com>
2024-01-06 13:46:55 -0700

    amps

--- src/main/java/org/bitbuckets/FieldConstants.java
+++ src/main/java/org/bitbuckets/FieldConstants.java
@@ -12,11 +12,15 @@ public class FieldConstants {
 
     static final double MIDWIDTH_TO_STEREO_METERS = 1.45;
 
+    static final double WALL_TO_AMP_LENGTH_METERS = 1.93;
+
     static final Translation2d BOTTOM_LEFT = new Translation2d(0,0);
     static final Translation2d TOP_LEFT = new Translation2d(FIELD_WIDTH_METERS, 0);
 
-    Translation2d BLUE_STEREO = new Translation2d((FIELD_WIDTH_METERS / 2d) + MIDWIDTH_TO_STEREO_METERS, 0);
-    Translation2d RED_STEREO = new Translation2d((FIELD_WIDTH_METERS / 2d) + MIDWIDTH_TO_STEREO_METERS, FIELD_LENGTH_METERS);
+    static final Translation2d BLUE_STEREO = new Translation2d((FIELD_WIDTH_METERS / 2d) + MIDWIDTH_TO_STEREO_METERS, 0);
+    static final Translation2d RED_STEREO = new Translation2d((FIELD_WIDTH_METERS / 2d) + MIDWIDTH_TO_STEREO_METERS, FIELD_LENGTH_METERS);
 
+    static final Translation2d BLUE_AMP = new Translation2d(FIELD_WIDTH_METERS, WALL_TO_AMP_LENGTH_METERS);
+    static final Translation2d RED_AMP = new Translation2d(FIELD_WIDTH_METERS, FIELD_LENGTH_METERS - WALL_TO_AMP_LENGTH_METERS);
 
 }
