61a2628c11e1f8538f5910a7e4bcf3216e99c4b0
louis <liu.louis91@gmail.com>
2024-01-06 17:56:34 -0700

    updated variable names

--- src/main/java/org/bitbuckets/FieldConstants.java
+++ src/main/java/org/bitbuckets/FieldConstants.java
@@ -10,8 +10,8 @@ public class FieldConstants {
     static final double FIELD_X_METERS = 16.54;
     static final double FIELD_Y_METERS = 8.02;
 
-    static final double Y_FROM_MIDPOINT_TO_STEREO = 1.45;
-    static final double X_FROM_WALL_TO_AMP = 1.93;
+    static final double Y_FROM_MIDPOINT_TO_STEREO_METERS = 1.45;
+    static final double X_FROM_WALL_TO_AMP_METERS = 1.93;
 
     static final double ALLIANCE_WALL_TO_STAGE_CENTROID_METERS = 3.07;
 
@@ -25,11 +25,11 @@ public class FieldConstants {
     static final Translation2d BOTTOM_LEFT = new Translation2d(0,0);
     static final Translation2d TOP_LEFT = new Translation2d(0, FIELD_Y_METERS);
 
-    static final Translation2d BLUE_STEREO = new Translation2d(0, (FIELD_Y_METERS / 2d) + Y_FROM_MIDPOINT_TO_STEREO);
-    static final Translation2d RED_STEREO = new Translation2d( FIELD_X_METERS, (FIELD_Y_METERS / 2d) + Y_FROM_MIDPOINT_TO_STEREO);
+    static final Translation2d BLUE_STEREO = new Translation2d(0, (FIELD_Y_METERS / 2d) + Y_FROM_MIDPOINT_TO_STEREO_METERS);
+    static final Translation2d RED_STEREO = new Translation2d( FIELD_X_METERS, (FIELD_Y_METERS / 2d) + Y_FROM_MIDPOINT_TO_STEREO_METERS);
 
-    static final Translation2d BLUE_AMP = new Translation2d(X_FROM_WALL_TO_AMP, FIELD_Y_METERS);
-    static final Translation2d RED_AMP = new Translation2d(FIELD_X_METERS - X_FROM_WALL_TO_AMP, FIELD_Y_METERS);
+    static final Translation2d BLUE_AMP = new Translation2d(X_FROM_WALL_TO_AMP_METERS, FIELD_Y_METERS);
+    static final Translation2d RED_AMP = new Translation2d(FIELD_X_METERS - X_FROM_WALL_TO_AMP_METERS, FIELD_Y_METERS);
 
     Translation2d BLUE_STAGE_CENTER = new Translation2d(ALLIANCE_WALL_TO_STAGE_CENTROID_METERS, (FIELD_Y_METERS / 2d));
     Translation2d RED_STAGE_CENTER = new Translation2d((FIELD_X_METERS - ALLIANCE_WALL_TO_STAGE_CENTROID_METERS), (FIELD_Y_METERS / 2d));
