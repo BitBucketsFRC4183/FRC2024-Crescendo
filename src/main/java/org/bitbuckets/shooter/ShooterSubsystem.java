1b5bacdc131f8e349eab84d83381af60233cee07
Ibrahim Bah <ib.bah2005@gmail.com>
2024-01-10 20:03:02 -0700

    created setMotorSpeeds method

--- src/main/java/org/bitbuckets/shooter/ShooterSubsystem.java
+++ src/main/java/org/bitbuckets/shooter/ShooterSubsystem.java
@@ -8,24 +8,32 @@ public class ShooterSubsystem implements Subsystem {
 
     // converts desired velocity into voltage
     final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(4,3);
-    final IRotationalMotor leftMotor;
-    final IRotationalMotor rightMotor;
+    final IRotationalMotor leftMotor1Wheel;
+    final IRotationalMotor rightMotor1Wheel;
+    final IRotationalMotor leftMotor2Wheel;
+    final IRotationalMotor rightMotor2Wheel;
 
-    public ShooterSubsystem(IRotationalMotor leftMotor, IRotationalMotor rightMotor) {
-        this.leftMotor = leftMotor;
-        this.rightMotor = rightMotor;
-
+    public ShooterSubsystem(IRotationalMotor leftMotor1Wheel, IRotationalMotor rightMotor1Wheel, IRotationalMotor leftMotor2Wheel, IRotationalMotor rightMotor2Wheel) {
+        this.leftMotor1Wheel = leftMotor1Wheel;
+        this.rightMotor1Wheel = rightMotor1Wheel;
+        this.leftMotor2Wheel = leftMotor2Wheel;
+        this.rightMotor2Wheel = rightMotor2Wheel;
+                
         register();
     }
 
-    public void setMotorSpeeds(double leftMotorSpeed, double rightMotorSpeed)
+    public void setMotorSpeeds(double leftMotorSpeed1Wheel, double rightMotorSpeed1Wheel, double leftMotorSpeed2Wheel, double rightMotorSpeed2Wheel)
     {
 
-        double leftVoltage = feedforward.calculate(leftMotorSpeed);
-        double rightVoltage = feedforward.calculate(rightMotorSpeed);
+        double leftVoltage1Wheel = feedforward.calculate(leftMotorSpeed1Wheel);
+        double rightVoltage1Wheel = feedforward.calculate(rightMotorSpeed1Wheel);
+        double leftVoltage2Wheel = feedforward.calculate(leftMotorSpeed2Wheel);
+        double rightVoltage2Wheel = feedforward.calculate(rightMotorSpeed2Wheel);
 
-        leftMotor.setToVoltage(leftVoltage);
-        rightMotor.setToVoltage(rightVoltage);
+        leftMotor1Wheel.setToVoltage(leftVoltage1Wheel);
+        rightMotor1Wheel.setToVoltage(rightVoltage1Wheel);
+        leftMotor2Wheel.setToVoltage(leftVoltage2Wheel);
+        rightMotor2Wheel.setToVoltage(rightVoltage2Wheel);
     }
 
 
