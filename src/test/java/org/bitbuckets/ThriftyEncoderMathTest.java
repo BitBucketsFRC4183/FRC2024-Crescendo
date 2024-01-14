8c536d4ed9ebe5819f6c38ca6ee6f1b81c81999c
DanielWhatsGood <daniel.changxu.wu@gmail.com>
2024-01-14 15:19:23 -0700

    Completed Unit Testing for ThriftyEncoder. Everything (probably) works!

--- src/test/java/org/bitbuckets/ThriftyEncoderMathTest.java
+++ src/test/java/org/bitbuckets/ThriftyEncoderMathTest.java
@@ -7,6 +7,7 @@ import org.bitbuckets.util.Util;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,32 +19,49 @@ public class ThriftyEncoderMathTest {
     @Test
     public void checkThriftyAngularPosition_encoderRotations()
     {
-        ThriftyEncoder thriftyEncoder = new ThriftyEncoder(Mockito.mock(AnalogInput.class), Mockito.mock(EncoderComponent.class));
+        AnalogInput fake = Mockito.mock(AnalogInput.class);
+        Mockito.when(fake.getVoltage()).thenReturn(-2.4);
 
-        assertEquals(0, thriftyEncoder.angularPosition_encoderRotations(), 0.1);
+        ThriftyEncoder thriftyEncoder = new ThriftyEncoder(fake, Mockito.mock(EncoderComponent.class));
+        assertEquals(-0.5, thriftyEncoder.angularPosition_encoderRotations(), 0);
     }
     
     
     @Test
     public void checkThriftyAngularPosition_normalizedEncoderRotations()
     {
-        ThriftyEncoder thriftyEncoder = new ThriftyEncoder(Mockito.mock(AnalogInput.class), Mockito.mock(EncoderComponent.class));        assertEquals(0, thriftyEncoder.angularPosition_normalizedEncoderRotations(), 0.1);
+        AnalogInput fake = Mockito.mock(AnalogInput.class);
+        Mockito.when(fake.getVoltage()).thenReturn(-4.8);
+
+        ThriftyEncoder thriftyEncoder = new ThriftyEncoder(fake, Mockito.mock(EncoderComponent.class));
+        assertEquals(0, thriftyEncoder.angularPosition_normalizedEncoderRotations(), 0);
     }
 
     
     @Test
     public void checkThriftyAngularPosition_mechanismRotations()
     {
-        ThriftyEncoder thriftyEncoder = new ThriftyEncoder(Mockito.mock(AnalogInput.class), Mockito.mock(EncoderComponent.class));
-        assertEquals(0, thriftyEncoder.angularPosition_mechanismRotations(), 0.1);
+        AnalogInput fake = Mockito.mock(AnalogInput.class);
+        Mockito.when(fake.getVoltage()).thenReturn(2d);
+        EncoderComponent component =  Mockito.mock(EncoderComponent.class);
+        Mockito.when(component.getEncoderToMechanismCoefficient()).thenReturn(3d);
+
+        ThriftyEncoder thriftyEncoder = new ThriftyEncoder(fake,component);
+
+        assertEquals(1.25, thriftyEncoder.angularPosition_mechanismRotations(), 0);
     }
 
     
     @Test
     public void checkThriftyAngularPosition_NormalizedMechanismRotations()
     {
-        ThriftyEncoder thriftyEncoder = new ThriftyEncoder(Mockito.mock(AnalogInput.class), Mockito.mock(EncoderComponent.class));
-        assertEquals(0, thriftyEncoder.angularPosition_normalizedMechanismRotations(), 0.1);
+        AnalogInput fake = Mockito.mock(AnalogInput.class);
+        Mockito.when(fake.getVoltage()).thenReturn(-2d);
+        EncoderComponent component =  Mockito.mock(EncoderComponent.class);
+        Mockito.when(component.getEncoderToMechanismCoefficient()).thenReturn(3d);
+
+        ThriftyEncoder thriftyEncoder = new ThriftyEncoder(fake, component);
+        assertEquals(0.75, thriftyEncoder.angularPosition_normalizedMechanismRotations(), 0.1);
     }
     
 
