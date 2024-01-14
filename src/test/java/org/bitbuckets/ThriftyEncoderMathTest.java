552cf2c7d75431c5cdf5a1e0ff70f8f9a2178f5c
DanielWhatsGood <daniel.changxu.wu@gmail.com>
2024-01-14 14:11:22 -0700

    Added ThriftyEncoderMathTest for Unit Testing the thrifty encoder (wip)

--- src/test/java/org/bitbuckets/ThriftyEncoderMathTest.java
+++ src/test/java/org/bitbuckets/ThriftyEncoderMathTest.java
@@ -1,13 +1,49 @@
 package org.bitbuckets;
 
+import org.bitbuckets.util.EncoderComponent;
+import org.bitbuckets.util.ThriftyEncoder;
 import org.bitbuckets.util.Util;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-@Disabled
+
 public class ThriftyEncoderMathTest {
 
+    ThriftyEncoder thriftyEncoder;
+
+    public ThriftyEncoderMathTest(ThriftyEncoder thriftyEncoder) {
+        this.thriftyEncoder = thriftyEncoder;
+    }
+
+    @Disabled
+    @Test
+    public void checkThriftyAngularPosition_encoderRotations()
+    {
+        assertEquals(0, thriftyEncoder.angularPosition_encoderRotations(), 0.1);
+    }
+    
+    @Disabled
+    @Test
+    public void checkThriftyAngularPosition_normalizedEncoderRotations()
+    {
+        assertEquals(0, thriftyEncoder.angularPosition_normalizedEncoderRotations(), 0.1);
+    }
+
+    @Disabled
+    @Test
+    public void checkThriftyAngularPosition_mechanismRotations()
+    {
+        assertEquals(0, thriftyEncoder.angularPosition_mechanismRotations(), 0.1);
+    }
+
+    @Disabled
+    @Test
+    public void checkThriftyAngularPosition_NormalizedMechanismRotations()
+    {
+        assertEquals(0, thriftyEncoder.angularPosition_normalizedMechanismRotations(), 0.1);
+    }
+    
 
 
 
