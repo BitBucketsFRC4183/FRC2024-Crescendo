3b85b8d40bb44ae93b25270433a0ac573e1e0957
DanielWhatsGood <daniel.changxu.wu@gmail.com>
2024-01-14 14:23:13 -0700

    fixed somethings inside thriftyEncoder

--- src/test/java/org/bitbuckets/ThriftyEncoderMathTest.java
+++ src/test/java/org/bitbuckets/ThriftyEncoderMathTest.java
@@ -1,44 +1,43 @@
 package org.bitbuckets;
 
+import edu.wpi.first.wpilibj.AnalogInput;
 import org.bitbuckets.util.EncoderComponent;
 import org.bitbuckets.util.ThriftyEncoder;
 import org.bitbuckets.util.Util;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class ThriftyEncoderMathTest {
 
-    ThriftyEncoder thriftyEncoder;
-
-    public ThriftyEncoderMathTest(ThriftyEncoder thriftyEncoder) {
-        this.thriftyEncoder = thriftyEncoder;
-    }
-
-    @Disabled
+    
     @Test
     public void checkThriftyAngularPosition_encoderRotations()
     {
-        assertEquals(0, thriftyEncoder.angularPosition_encoderRotations(), 0.1);
+        ThriftyEncoder encoder = new ThriftyEncoder(Mockito.mock(AnalogInput.class), Mockito.mock(EncoderComponent.class))
+
+        assertEquals(0, encoder.angularPosition_encoderRotations(), 0.1);
     }
     
-    @Disabled
+    
     @Test
     public void checkThriftyAngularPosition_normalizedEncoderRotations()
     {
         assertEquals(0, thriftyEncoder.angularPosition_normalizedEncoderRotations(), 0.1);
     }
 
-    @Disabled
+    
     @Test
     public void checkThriftyAngularPosition_mechanismRotations()
     {
         assertEquals(0, thriftyEncoder.angularPosition_mechanismRotations(), 0.1);
     }
 
-    @Disabled
+    
     @Test
     public void checkThriftyAngularPosition_NormalizedMechanismRotations()
     {
