b03bd90cfa45e75fd06d578a396a1d4d694cf7ff
louis <liu.louis91@gmail.com>
2024-01-14 19:07:40 -0700

    finished making default ground sysftem intake command

--- src/main/java/org/bitbuckets/commands/groundIntake/DefaultGroundIntakeCommand.java
+++ src/main/java/org/bitbuckets/commands/groundIntake/DefaultGroundIntakeCommand.java
@@ -1,6 +1,35 @@
 package org.bitbuckets.commands.groundIntake;
 
-public class DefaultGroundIntakeCommand {
+import edu.wpi.first.wpilibj2.command.Command;
+import org.bitbuckets.groundIntake.GroundIntakeSubsystem;
+
+public class DefaultGroundIntakeCommand extends Command {
+
+    private final GroundIntakeSubsystem groundIntakeSubsystem;
+
+    public DefaultGroundIntakeCommand(GroundIntakeSubsystem groundIntakeSubsystem) {
+        this.groundIntakeSubsystem = climberSubsystem;
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute() {
+        groundIntakeSubsystem.setMotorsZero();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 
 
 
