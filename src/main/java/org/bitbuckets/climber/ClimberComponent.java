3f09ebe35765b989f517b78877164a39b8e6417c
Matt Lui <92664603+superyuuki@users.noreply.github.com>
2024-01-21 02:44:37 -0700

    fix: make config more neat using inline tables fix: remove dampener

--- src/main/java/org/bitbuckets/climber/ClimberComponent.java
+++ src/main/java/org/bitbuckets/climber/ClimberComponent.java
@@ -4,9 +4,7 @@ import xyz.auriium.mattlib2.log.INetworkedComponent;
 import xyz.auriium.mattlib2.log.annote.Conf;
 
 public interface ClimberComponent extends INetworkedComponent {
-    @Conf("ks") double ff_ks(); // TODO
-    @Conf("kv") double ff_kv(); // TODO
-
-    @Conf("dampener") double dampener(); // TODO
+    @Conf("ff_ks") double ff_ks(); // TODO
+    @Conf("ff_kv") double ff_kv(); // TODO
 
 }
