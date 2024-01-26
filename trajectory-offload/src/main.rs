5ecb86827e10320aaf865e11746500fdaaa7c982
Matt Lui <92664603+superyuuki@users.noreply.github.com>
2024-01-25 21:29:31 -0700

    add: ability to cal trajopt from main rust daemon

---
+++ trajectory-offload/src/main.rs
@@ -0,0 +1,9 @@
+use navigation::{initialize_solver, solve};
+
+fn main() {
+    let mut alm_cache = initialize_solver();
+    //solve(&[2,2], &alm_cache, [1,2], Option::None, Option::None);
+
+
+    println!("Hello, world!");
+}
