5bf92a26ba3d64ec0d4044f1382788e2aa4e8db1
sirgwain <sirgwain@gmail.com>
2024-01-28 14:37:42 -0700

    Updated tests

--- src/test/java/org/bitbuckets/drive/SwerveModuleTest.java
+++ src/test/java/org/bitbuckets/drive/SwerveModuleTest.java
@@ -1,5 +1,8 @@
 package org.bitbuckets.drive;
 
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.AdditionalMatchers;
@@ -7,6 +10,7 @@ import xyz.auriium.mattlib2.hardware.ILinearMotor;
 import xyz.auriium.mattlib2.hardware.IRotationEncoder;
 import xyz.auriium.mattlib2.hardware.IRotationalController;
 
+import static org.bitbuckets.RobotContainer.SWERVE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
@@ -15,12 +19,15 @@ public class SwerveModuleTest {
     ILinearMotor driveMotor;
     IRotationalController steerController;
     IRotationEncoder absoluteEncoder;
+    SimpleMotorFeedforward ff;
 
     @BeforeEach
     public void beforeEach() {
         driveMotor = mock(ILinearMotor.class);
         steerController = mock(IRotationalController.class);
         absoluteEncoder = mock(IRotationEncoder.class);
+        ff = new SimpleMotorFeedforward(SWERVE.ff_ks(), SWERVE.ff_kv());
+
     }
 
     @Test
@@ -36,42 +43,42 @@ public class SwerveModuleTest {
 
     @Test
     public void testSetToMoveAtStraight() {
-        SwerveModule module = new SwerveModule(driveMotor, steerController, absoluteEncoder);
+        SwerveModule module = new SwerveModule(driveMotor, steerController, absoluteEncoder, ff);
 
-        module.setToMoveAt(12, 0);
+        module.setToMoveAt(new SwerveModuleState(20, Rotation2d.fromDegrees(0)));
         verify(driveMotor).setToVoltage(12);
         verify(steerController).controlToNormalizedReference(0);
     }
 
     @Test
     public void testSetToMoveAtTurn1() {
-        SwerveModule module = new SwerveModule(driveMotor, steerController, absoluteEncoder);
+        SwerveModule module = new SwerveModule(driveMotor, steerController, absoluteEncoder, ff);
 
         // move a quarter
-        module.setToMoveAt(12, .25);
+        module.setToMoveAt(new SwerveModuleState(20, Rotation2d.fromDegrees(90)));
         verify(steerController).controlToNormalizedReference(.25);
     }
 
     @Test
     public void testSetToMoveAtTurn2() {
-        SwerveModule module = new SwerveModule(driveMotor, steerController, absoluteEncoder);
+        SwerveModule module = new SwerveModule(driveMotor, steerController, absoluteEncoder, ff);
 
         // move a quarter
-        module.setToMoveAt(12, -.25);
+        module.setToMoveAt(new SwerveModuleState(20, Rotation2d.fromDegrees(-90)));
         verify(steerController).controlToNormalizedReference(.75);
     }
 
     @Test
     public void testSetToMoveAtTurn3() {
-        SwerveModule module = new SwerveModule(driveMotor, steerController, absoluteEncoder);
+        SwerveModule module = new SwerveModule(driveMotor, steerController, absoluteEncoder, ff);
 
         // abs encoder registers 1/2 turn clockwise
-        when(absoluteEncoder.angularPosition_normalizedMechanismRotations()).thenReturn(.5);
+        when(steerController.angularPosition_normalizedMechanismRotations()).thenReturn(.5);
 
         // rotate a bit counter-clockwise
         // the "asked for" rotation would be -.7, but we flip the drive motor in reverse and only
         // actually rotate .3
-        module.setToMoveAt(12, -.2);
+        module.setToMoveAt(new SwerveModuleState(20, Rotation2d.fromDegrees(-72)));
 
         // the drive motor should move backwards, and the mechanism should be set to .4 (-.1 + .5)
         verify(driveMotor).setToVoltage(-12);
@@ -80,13 +87,13 @@ public class SwerveModuleTest {
 
     @Test
     public void testSetToMoveAtTurn4() {
-        SwerveModule module = new SwerveModule(driveMotor, steerController, absoluteEncoder);
+        SwerveModule module = new SwerveModule(driveMotor, steerController, absoluteEncoder, ff);
 
         // abs encoder registers 1/4 turn clockwise
-        when(absoluteEncoder.angularPosition_normalizedMechanismRotations()).thenReturn(.25);
+        when(steerController.angularPosition_normalizedMechanismRotations()).thenReturn(.25);
 
         // rotate a bit counter-clockwise
-        module.setToMoveAt(12, -.1);
+        module.setToMoveAt(new SwerveModuleState(20, Rotation2d.fromDegrees(-36)));
 
         // the drive motor should move backwards, and the mechanism should be set to .4 (-.1 + .5)
         verify(driveMotor).setToVoltage(-12);
