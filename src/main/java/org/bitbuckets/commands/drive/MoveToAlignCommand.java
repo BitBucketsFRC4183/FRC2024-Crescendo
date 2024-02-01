cb04586d6a9c2b9687c9d6bc9adb6af098ecf96b
Titus Nguyen <titus.tnguyen17@gmail.com>
2024-01-31 20:13:08 -0700

    MoveToAlign finish condition

--- src/main/java/org/bitbuckets/commands/drive/MoveToAlignCommand.java
+++ src/main/java/org/bitbuckets/commands/drive/MoveToAlignCommand.java
@@ -1,14 +1,14 @@
 package org.bitbuckets.commands.drive;
 
 import edu.wpi.first.math.controller.HolonomicDriveController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.*;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import org.bitbuckets.OperatorInput;
 import org.bitbuckets.drive.DriveSubsystem;
 import org.bitbuckets.drive.OdometrySubsystem;
 import org.bitbuckets.vision.VisionSubsystem;
+import java.util.Optional;
 
 public class MoveToAlignCommand extends Command {
 
@@ -18,13 +18,18 @@ public class MoveToAlignCommand extends Command {
     final OdometrySubsystem odometrySubsystem;
     final OperatorInput operatorInput;
 
+    Pose3d targetPose;
 
-    public MoveToAlignCommand(DriveSubsystem driveSubsystem, VisionSubsystem visionSubsystem, HolonomicDriveController holoControlleer, OdometrySubsystem odometrySubsystem, OperatorInput operatorInput) {
+
+    public MoveToAlignCommand(DriveSubsystem driveSubsystem, VisionSubsystem visionSubsystem, HolonomicDriveController holoController, OdometrySubsystem odometrySubsystem, OperatorInput operatorInput) {
         this.driveSubsystem = driveSubsystem;
         this.visionSubsystem = visionSubsystem;
-        this.holoController = holoControlleer;
+        this.holoController = holoController;
         this.odometrySubsystem = odometrySubsystem;
         this.operatorInput = operatorInput;
+
+        // change this with visiontarget implementation later
+        this.targetPose = new Pose3d(0, 0 ,0, new Rotation3d(0, 0 ,0));
     }
 
     @Override
@@ -34,31 +39,37 @@ public class MoveToAlignCommand extends Command {
 
     @Override
     public void execute() {
+        // this must be updated every as frequently as possible
+        Optional<Pose3d> optionalTagPose = visionSubsystem.estimateBestVisionTarget_1();
+        optionalTagPose.ifPresent(pose3d -> this.targetPose = pose3d);
         moveToAlign();
     }
     public ChassisSpeeds calculateTagSpeeds(Pose2d target, Rotation2d holonomicRotation, double desiredVelocity) {
-
-
-        ChassisSpeeds speed = holoController.calculate(
+        return holoController.calculate(
                 odometrySubsystem.getRobotCentroidPosition(),
                 target,
                 desiredVelocity,
                 holonomicRotation
         );
-        return speed;
     }
 
     public void moveToAlign() {
-        var tagPose_1 = visionSubsystem.estimateBestVisionTarget_1();
-        if (tagPose_1.isPresent()) {
-            ChassisSpeeds speeds = calculateTagSpeeds(tagPose_1.get().toPose2d(), tagPose_1.get().toPose2d().getRotation().plus(Rotation2d.fromDegrees(180)), 1);
+        ChassisSpeeds speeds = calculateTagSpeeds(this.targetPose.toPose2d(), this.targetPose.toPose2d().getRotation().plus(Rotation2d.fromDegrees(180)), 1);
+        driveSubsystem.driveUsingChassisSpeed(speeds);
 
-            driveSubsystem.driveUsingChassisSpeed(speeds);
-        }
     }
 
     @Override
     public void end(boolean interrupted) {
         driveSubsystem.commandWheelsToZero();
+    }
+
+    @Override
+    public boolean isFinished() {
+        Pose2d currentPose = odometrySubsystem.getRobotCentroidPosition();
+
+        return Math.abs(currentPose.getX() - targetPose.getY()) < 0.05 &&
+                Math.abs(currentPose.getX() - targetPose.getY()) < 0.05 &&
+                (currentPose.getRotation().getDegrees() - targetPose.getRotation().getAngle()) < 5;
     }
 }
