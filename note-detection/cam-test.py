db7d362e4f2b65319725e4efd6dd1c2a611a9e5a
Titus Nguyen <titus.tnguyen17@gmail.com>
2024-03-13 19:12:26 -0700

    bye bye

--- note-detection/cam-test.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-cam = cv2.VideoCapture(0)
-
-ret, frame = cam.read()
-
-cam.release()
-
-cv2.imwrite("captured_img.jpg", frame)
--- note-detection/mt-detect/rkcat.sh
+++
@@ -1,5 +0,0 @@
-# 查看温度
-sensors
-# 查看NPU占用
-echo "当前NPU占用:"
-sudo cat /sys/kernel/debug/rknpu/load
