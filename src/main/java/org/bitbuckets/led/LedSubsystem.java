f70719342804bed6147ab4df73d7fc0dd8e5be76
Titus Nguyen <titus.tnguyen17@gmail.com>
2024-03-21 11:43:49 -0700

    maybe?

--- src/main/java/org/bitbuckets/led/LedSubsystem.java
+++ src/main/java/org/bitbuckets/led/LedSubsystem.java
@@ -1,5 +1,6 @@
 package org.bitbuckets.led;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -9,6 +10,8 @@ import org.bitbuckets.RobotContainer;
 import org.bitbuckets.noteManagement.NoteManagementSubsystem;
 import xyz.auriium.mattlib2.loop.IMattlibHooked;
 
+import java.lang.reflect.Array;
+
 
 public class LedSubsystem implements Subsystem, IMattlibHooked {
 
@@ -17,17 +20,24 @@ public class LedSubsystem implements Subsystem, IMattlibHooked {
     final AddressableLEDBuffer buffer;
 
     int rainbowFirstPixelHue = 0;
-    int PWM_header = 9;
-    final boolean lastState;
 
+    int PWM_header = 9;
+    boolean lastState;
+
+    ledMode currentMode;
+    ledMode lastMode;
+
+    public enum ledMode {
+        RAINBOW,
+        SOLID,
+        GRADIENT
+    }
 
     public LedSubsystem(NoteManagementSubsystem nms) {
         this.nms = nms;
         this.ledStrip = new AddressableLED(PWM_header);
         this.buffer = new AddressableLEDBuffer(60);
         ledStrip.setLength(buffer.getLength());
-
-        this.lastState = nms.isNoteIn();
 
         setBufferColor(Color.kAntiqueWhite);
         ledStrip.setData(buffer);
@@ -41,9 +51,11 @@ public class LedSubsystem implements Subsystem, IMattlibHooked {
     @Override
     public void logicPeriodic() {
         if (!DriverStation.isTeleopEnabled()) {
+            this.currentMode = ledMode.RAINBOW;
             rainbow();
         } else {
-            if (this.lastState != this.nms.isNoteIn()) {
+            this.currentMode = ledMode.SOLID;
+            if (this.lastState != this.nms.isNoteIn() || ((this.currentMode == ledMode.SOLID) && (this.lastMode != this.currentMode))) {
                 if (this.nms.isNoteIn()) {
                     setBufferColor(Color.kHoneydew);
                 } else setBufferColor(Color.kMagenta);
@@ -51,10 +63,13 @@ public class LedSubsystem implements Subsystem, IMattlibHooked {
         }
 
         ledStrip.setData(buffer);
+        this.lastState = this.nms.isNoteIn();
+        this.lastMode = this.currentMode;
+        RobotContainer.LED.log_current_mode(this.currentMode.toString());
     }
 
     private void setBufferColor(Color color) {
-        RobotContainer.LED.log_current_mode("solid-color");
+        this.currentMode = ledMode.SOLID;
         for (var i = 0; i < buffer.getLength(); i++) {
             buffer.setLED(i, color);
         }
@@ -62,7 +77,7 @@ public class LedSubsystem implements Subsystem, IMattlibHooked {
 
     private void rainbow() {
         // For every pixel
-        RobotContainer.LED.log_current_mode("rainbow");
+
         for (var i = 0; i < buffer.getLength(); i++) {
             // Calculate the hue - hue is easier for rainbows because the color
             // shape is a circle so only one value needs to precess
@@ -76,5 +91,28 @@ public class LedSubsystem implements Subsystem, IMattlibHooked {
         // Check bounds
         rainbowFirstPixelHue %= 180;
     }
+
+    private void lerpGradient(Color[] colors) {
+        this.current_mode = ledMode.GRADIENT;
+        RobotContainer.LED.log_current_mode("gradient");
+        // per color, create a gradient
+        // i = color
+        // j = led #
+        for (var i = 0; i < colors.length; i++) {
+            for (var j = 0; j < buffer.getLength()/colors.length; j++) {
+                Color currentColor = colors[i];
+                Color nextColor = colors[i + 1];
+                // Calculate the hue - hue is easier for rainbows because the color
+                // shape is a circle so only one value needs to precess
+        }
+        // Increase by to make the rainbow "move"
+        var speed = 3;
+        rainbowFirstPixelHue += speed;
+        // Check bounds
+        rainbowFirstPixelHue %= 180;
+    }
+
 }
+
+
 
